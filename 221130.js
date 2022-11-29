b172d323e548fc3652be6c8421ea0fd96f06f314
Tom Ko <yungsu2391@gmail.com>
2022-11-30 08:34:58 +0900

    221130 morning

--- 221130.js
+++ 221130.js
@@ -1,33 +1,6 @@
 // const fs = require('fs');
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const input = `3
-1
-4
-5
-7
-9
-6
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30`;
+
 // const T = Number(input.split("\n"));
 // const arr = input.split("\n")[1].split(' ');
 // let min = Number(arr[0]);
@@ -50,8 +23,69 @@ const input = `3
 // }
 // console.log(max);
 // console.log(index);
-const arr = input.split("\n");
-const arrMap = arr.map(Number);
-for(let i = 1; i<= 30; i++){
-    if(arrMap.indexOf(i,0) == -1) console.log(i)
+// const arr = input.split("\n");
+// const arrMap = arr.map(Number);
+// for(let i = 1; i<= 30; i++){
+//     if(arrMap.indexOf(i,0) == -1) console.log(i)
+// }
+
+// const arr = input.split("\n");
+// const arrMap = arr.map(Number);
+// let resultArr = [];
+// for(let i = 1; i<= arrMap.length; i++){
+//     resultArr.push(arrMap[i] % 42);
+// }
+// const set = new Set(resultArr);
+// const result = [...set];
+// console.log(result.length);
+
+// const input = `5
+// OOXXOXXOOO
+// OOXXOOXXOO
+// OXOXOXOXOXOXOX
+// OOOOOOOOOO
+// OOOOXOOOOXOOOOX`;
+
+// const T = Number(input.split("\n"));
+// const arr = input.split("\n")[1].split(' ');
+// const numArr = arr.map(Number);
+
+// // max를 찾는다.
+// let max = numArr[0];
+// let total = 0;
+// for(let i = 0; i< numArr.length; i++){
+//     if(max < numArr[i]) max =  numArr[i];
+// }
+
+// for(let i = 0; i< numArr.length; i++){
+//     total += numArr[i];
+// }
+// console.log(total);
+// console.log(total/numArr.length/max*100);
+
+const input = `5
+OOXXOXXOOO
+OOXXOOXXOO
+OXOXOXOXOXOXOX
+OOOOOOOOOO
+OOOOXOOOOXOOOOX`;
+const input2 = input.split("\n");
+const T = Number(input2.shift(0));
+
+
+for(let i = 0; i<T; i++){
+    let total = 0;
+    for(let j = 0; j<input2[i].length; j++){
+        let score = 0;
+        if(input2[i][j] == 'O'){
+            let iter = j;
+            while(input2[i][iter] == 'O'){
+                score += 1;
+                iter -= 1;
+            }
+            console.log(score);
+        }
+        total+=score;
+    }
+    console.log("total",total);
 }
